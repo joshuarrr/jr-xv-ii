9bf9fa643c523a8f3b4a5da4137e4ba4665c899c
Joshua Richey <joshua@idealist.org>
2015-11-01 10:15:37 -0800

    Simple routes.

--- app/main.js
+++ app/main.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { Router, Route, Link } from 'react-router'
 import { Home } from './content/home';
+import { About } from './content/about';
 require("./styles/app.css");
 
 export class App extends Component {
   render() {
     return (
-      <div>
-        <Home />
-      </div>
+			<div>
+				<nav>
+				  <ul>
+				    <li><Link to="/home">Home</Link></li>
+				    <li><Link to="/about">About</Link></li>
+				  </ul>
+				</nav>
+				{this.props.children}
+			</div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render((
+  <Router>
+    <Route path="/" component={App}>
+      <Route path="about" component={About} />
+      <Route path="home" component={Home} />
+    </Route>
+  </Router>
+), document.getElementById('root'))
