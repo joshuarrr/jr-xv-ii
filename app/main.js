8cca2a1938792ec86a44ad0384d1323d824f6ff6
Joshua Richey <joshua@idealist.org>
2015-10-31 23:05:13 -0700

    Cleanup + added Fira.

---
+++ app/main.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { Home } from './content/home';
+require("./styles/app.css");
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Home />
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
