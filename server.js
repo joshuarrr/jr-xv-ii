cd5d029f0ddba875d7a8c7598071d86fb132921a
Joshua Richey <joshua@idealist.org>
2015-11-07 14:18:43 -0800

    Server config

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ var SampleApp = function() {
         }
 
         //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('./build/index.html');
+        self.zcache['index.html'] = fs.readFileSync('./index.html');
 
         var dist = fs.readdirSync('./dist');
         dist.forEach(function(asset) {
