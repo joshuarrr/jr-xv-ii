e9ff43b79b49405e1a1ab30f92a16c043a85ff8a
Joshua Richey <joshua@idealist.org>
2015-11-07 12:57:55 -0800

    Prod config.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,10 +1,39 @@
 var path = require('path');
 var webpack = require('webpack');
 
-module.exports = {
+// PostCSS
+var cssimport = require('postcss-import');
+var autoprefixer = require('autoprefixer');
+var calc = require('postcss-calc');
+var colorFunction = require('postcss-color-function');
+var conditionals = require('postcss-conditionals');
+var cssVariables = require('postcss-css-variables');
+var customMedia = require('postcss-custom-media');
+var customProperties = require('postcss-custom-properties');
+var discardComments = require('postcss-discard-comments');
+var mediaMinMax = require('postcss-media-minmax');
+var mixins = require('postcss-mixins');
+var nested = require('postcss-nested');
+var browserReporter = require('postcss-browser-reporter');
+var reporter = require('postcss-reporter');
+var simpleExtend = require('postcss-simple-extend');
+var simpleVars = require('postcss-simple-vars');
+var stylelint = require('stylelint');
+const AUTOPREFIXER_BROWSERS = [
+  'Android 2.3',
+  'Android >= 4',
+  'Chrome >= 20',
+  'Firefox >= 24',
+  'Explorer >= 8',
+  'iOS >= 6',
+  'Opera >= 12',
+  'Safari >= 6'
+];
+
+var config = {
   devtool: 'source-map',
   entry: [
-    './src/index'
+    './app/main.js'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
@@ -24,11 +53,58 @@ module.exports = {
       }
     })
   ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src')
-    }]
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'app')
+      }, {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader!postcss-loader'
+      }, {
+        test: /\.(png|jpeg|woff|woff2|eot|ttf)$/,
+        loader: 'url-loader?limit=8192'
+      },
+      {
+      test: /\.svg$/,
+      loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+      }
+    ]
+  },
+  postcss: function () {
+    return [
+      cssimport({
+        onImport: function (files) {
+          files.forEach(this.addDependency);
+        }.bind(this)
+      }),
+      stylelint,
+      discardComments,
+      colorFunction,
+      mixins,
+      simpleExtend,
+      customProperties,
+      cssVariables,
+      simpleVars,
+      customMedia,
+      mediaMinMax,
+      nested,
+      calc,
+      conditionals,
+      autoprefixer(AUTOPREFIXER_BROWSERS),
+      browserReporter({
+        selector: 'body:before'
+      }),
+      reporter({
+        clearMessages: true,
+        noPlugin: true
+      }),
+    ];
   }
 };
+
+module.exports = config;
