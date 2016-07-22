4e825bfe4c0307b56af6d118d5c151ee72e30bf9
Joshua Richey <joshua@idealist.org>
2016-07-22 15:02:04 -0700

    Build

--- app/content/photography.jsx
+++ app/content/photography.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Helmet from "react-helmet";
 import { PageWrapper } from "./components/page-wrapper";
-import { Infinigram } from "./components/infinigram";
 require("../styles/content/photography.css");
 
 export class Photography extends Component {
@@ -12,7 +11,7 @@ export class Photography extends Component {
 				mainClass="photography"
 			>
 				<Helmet title="Photography" />
-				<Infinigram />
+				<p className="intro-text no-photos">There are no photos here right now because stupid Instagram changed their stupid api the day after I spent like a month trying to figure out how to write a decent lazy-loading infinite scrolling photo viewer. It was pretty cool, but now I don't have a photo section. I'll make something new pretty soon. I guess.</p>
 			</PageWrapper>
 		);
 	}
