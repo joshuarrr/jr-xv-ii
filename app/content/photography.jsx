2f69e477079502621fb66e1db28555bf62a5c537
Joshua Richey <joshua@idealist.org>
2016-07-31 16:18:59 -0700

    wip

--- app/content/photography.jsx
+++ app/content/photography.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import ReactDOM from "react-dom";
 import Helmet from "react-helmet";
 import { PageWrapper } from "./components/page-wrapper";
 import { ResponsiveContainer } from "./components/responsive-container";
@@ -7,19 +8,56 @@ import photos from '../data/photos.js';
 require("../styles/content/photography.css");
 
 export class Photography extends Component {
+	constructor() {
+		super();
+		this.state = {
+			scrolled: 'false'
+		};
+	}
 
-	render() {
+	// componentDidMount() {
+		// var image = document.querySelector('.banner');
+		// change the image height on scroll
+		// window.addEventListener("scroll", function(event) {
+		//   var banner = document.querySelector('.banner');
+		//   var top = this.scrollY;
+		//   console.log('top = ' + top);
+		//   var verticalScroll = document.querySelector(".page");
+		//   // console.log('Scroll Y: ' + top + "px");
+		//   var imgHeight = banner.clientHeight;
+		//   console.log('imgHeight = ' + imgHeight);
+		//   var i = top * .1;
+		//   console.log('i = ' + i);
+		//   var computedHeight = imgHeight - (top * .8);
+		//   console.log('computedHeight = ' + computedHeight);
+		//   banner.style.height = computedHeight + 'px';
+		// }, false);
+	// }
+
+	handleScroller() {
+		// debugger;
+		// console.dir( this.refs );
+	}
+
+	render = () => {
 		const filetype = 'jpg';
+		console.log('* this.state.scrolled = ' + this.state.scrolled + '\n');
+
 		const fullFrameImages = photos.map(function exp(photo, index) {
 			return (
-				<ResponsiveContainer class="ff-image">
+				<ResponsiveContainer
+					class="ff-image"
+					key={ 'ff-image' + index }
+					ref={ photo.file }
+					onScroll={ this.handleScroller() }
+				>
 					<ResponsiveImage
 						class="full-frame-image"
 						src={	photo.file + '.' + filetype}
 					/>
 				</ResponsiveContainer>
 			);
-		});
+		}, this);
 
 		return (
 			<PageWrapper
